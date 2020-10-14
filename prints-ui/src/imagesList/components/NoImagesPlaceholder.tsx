5fe49039a6f6906f264cdf390f15f926d98317b6
Dawid Czarczynski <dawidczarczynski@gmail.com>
2020-10-14 22:00:02 +0200

    Add no images placeholder

---
+++ prints-ui/src/imagesList/components/NoImagesPlaceholder.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NoImagesPlaceholder() {
+    return (
+        <div className="d-flex justify-content-center align-items-center">
+            <p>No images selected</p>
+        </div>
+    );
+}
